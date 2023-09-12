aed15dbd5f3c13306039516cded874d8e57e82a9
Jack Harvison <jackkharvison@gmail.com>
2023-09-12 16:48:27 -0700

    cleaned up destination setting

--- Door.gd
+++ Door.gd
@@ -1,12 +1,6 @@
 class_name Door extends Interaction
 
 @export var destination = ""
-var dest_scene 
-
-func _ready():
-	dest_scene = load(destination)
-
 	
 func _interact():
-#	pass
-	get_tree().change_scene_to_packed(dest_scene)
+	get_tree().change_scene_to_file(destination)
